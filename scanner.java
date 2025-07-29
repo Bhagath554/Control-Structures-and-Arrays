af3788d240474f5a5c31a017a4b66c537f3f025e
sureshsk9 <suresh@settleking.com>
2025-07-29 19:02:18 +0530

    P-2

---
+++ scanner.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class  scanner {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Hello user please enter Your Name:");
+        String name=sc.nextLine();
+        System.out.println("Enter your lucky number");
+        int lucky=sc.nextInt();
+        System.out.println("Hello "+name+",Your Lucky number"+lucky+"shows you are really hardworking.keepit up");
+    }
+
+    
+}
