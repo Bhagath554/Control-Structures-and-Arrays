e83c4707cbcefdf2bc1ef821ca31e0349cc80a37
sureshsk9 <suresh@settleking.com>
2025-07-29 18:52:25 +0530

    Control Structures and Arrays
    P-1

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=10;i>1;i--){
+            System.out.println("The value of i is :"+i);
+        }
+        int j=10;
+        while(j<11){
+            System.out.println("The value of i:"+j);
+            j++;
+        }
+    }
+}
